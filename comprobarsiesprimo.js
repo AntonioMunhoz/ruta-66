9a899b637c8776eff1363c4f3cb93270c6cba4ac
AntonioMunhoz <amunluc1705@g.educaand.es>
2025-02-17 12:30:51 +0100

    Update comprobarsiesprimo.js

--- comprobarsiesprimo.js
+++ comprobarsiesprimo.js
@@ -3,15 +3,14 @@ function comprobarEsPrimo (numero)
   var esPrimo;
   var i;
 
-for (i=numero-1; i>=2; i=i-1 )
-  {
-if ( numero % i == 0 )
-{
-  esPrimo = false;
-}
-
-  
-  }
-esPrimo = true; 
-return esPrimo;
+  for (i=numero-1; i>=2; i=i-1 )
+    {
+      if ( numero % i == 0 )
+        {
+        esPrimo = false;  
+        }
+    }
+  // si no hemos encontrado ningún divisor entonces el numero es primo
+  esPrimo = true;
+  return esPrimo;
 }
