cc8929a310ba3d38f21da33f80a9d6653946d009
AntonioMunhoz <amunluc1705@g.educaand.es>
2025-03-17 12:39:29 +0100

    Update numeros2.js

--- numeros2.js
+++ numeros2.js
@@ -8,12 +8,12 @@ do {
 return numero;
 }
 
-function pedirNumeroEnRangoMejorado(min, max) {
+function pedirNumeroEnRangoMejorado(texto,min,max) 
+{
 var numero;
 do {
-  numero = prompt("Mete un numero entre " + min +" y " + max + ":");
+  numero = prompt(texto + min +" y " + max + ":");
   numero = Number(numero);
 } while (numero < min || numero > max);
-
 return numero;
 }
