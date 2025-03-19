47568d616149e062a12119f57383507a328784c7
AntonioMunhoz <amunluc1705@g.educaand.es>
2025-03-19 13:38:02 +0100

    Create pedirNumero.js

---
+++ pedirNumero.js
@@ -0,0 +1,19 @@
+function pedirNumeroEnRango(min, max)
+  {
+  var numero;
+  do  {
+      numero = prompt("Ingrese un número entre " + min +" y " + max + ":");
+      numero = Number(numero); 
+      } while (numero < min || numero > max);
+  return numero;
+  }
+
+function pedirNumeroEnRangoMejorado (texto,min,max)
+ {
+  var numero;
+  do  {
+      numero = prompt(texto + min +" y " + max + ":");
+      numero = Number(numero); 
+      } while (numero < min || numero > max);
+  return numero;
+  }
